dcfd135ebc5c3f08e4f128e76e843e0624df6365
Caratacus <330952486@qq.com>
2018-10-24 16:22:32 +0800

    update mybatis plus version

--- src/main/java/org/crown/mapper/TestMapper.java
+++ src/main/java/org/crown/mapper/TestMapper.java
@@ -3,7 +3,8 @@ package org.crown.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.crown.model.Test;
 
-import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
 
 @Mapper
 public interface TestMapper extends BaseMapper<Test> {
