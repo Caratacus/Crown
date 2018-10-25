76f5cd9d1097977a09a4f888272a59ba395cab41
Caratacus <330952486@qq.com>
2018-10-25 12:47:59 +0800

    Change: TODO travis CI

---
+++ src/test/resources/sql/crown.sql
@@ -0,0 +1,127 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 50718
+ Source Host           : localhost
+ Source Database       : crown
+
+ Target Server Type    : MySQL
+ Target Server Version : 50718
+ File Encoding         : utf-8
+
+ Date: 10/25/2018 12:46:14 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `sys_menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL COMMENT '父菜单ID，一级菜单为0',
+  `menu_name` varchar(32) DEFAULT NULL COMMENT '菜单名称',
+  `path` varchar(64) DEFAULT NULL COMMENT '路径',
+  `menu_type` smallint(2) DEFAULT NULL COMMENT '类型:0:目录,1:菜单,2:按钮',
+  `icon` varchar(32) DEFAULT NULL COMMENT '菜单图标',
+  `create_uid` int(11) NOT NULL COMMENT '创建者ID',
+  `update_uid` int(11) NOT NULL COMMENT '修改者ID',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜单表';
+
+-- ----------------------------
+--  Table structure for `sys_menu_resource`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu_resource`;
+CREATE TABLE `sys_menu_resource` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_id` int(11) DEFAULT NULL COMMENT '菜单ID',
+  `resource_id` int(11) DEFAULT NULL COMMENT '资源ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜单资源关系表';
+
+-- ----------------------------
+--  Table structure for `sys_resource`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_resource`;
+CREATE TABLE `sys_resource` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `resource_name` varchar(32) DEFAULT NULL COMMENT '资源名称',
+  `mapping` varchar(64) DEFAULT NULL COMMENT '路径映射',
+  `method` varchar(6) DEFAULT NULL COMMENT '请求方式',
+  `verify` bit(1) DEFAULT NULL COMMENT '是否需要验证',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='资源表';
+
+-- ----------------------------
+--  Table structure for `sys_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(64) NOT NULL COMMENT '角色名称',
+  `create_uid` int(11) NOT NULL COMMENT '创建者ID',
+  `update_uid` int(11) NOT NULL COMMENT '修改者ID',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  `remark` varchar(128) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
+
+-- ----------------------------
+--  Table structure for `sys_role_menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL COMMENT '角色ID',
+  `menu_id` int(11) NOT NULL COMMENT '菜单ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色菜单关系表';
+
+-- ----------------------------
+--  Table structure for `sys_role_resource`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_resource`;
+CREATE TABLE `sys_role_resource` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL COMMENT '角色ID',
+  `resource_id` int(11) NOT NULL COMMENT '菜单ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色资源关系表';
+
+-- ----------------------------
+--  Table structure for `sys_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `nike_name` varchar(50) NOT NULL COMMENT '用户名',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(100) DEFAULT NULL COMMENT '手机',
+  `status` smallint(2) DEFAULT NULL COMMENT '状态 0：禁用 1：正常',
+  `create_uid` int(11) NOT NULL COMMENT '创建者ID',
+  `update_uid` int(11) NOT NULL COMMENT '修改者ID',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统用户表';
+
+-- ----------------------------
+--  Table structure for `sys_user_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) DEFAULT NULL COMMENT '用户ID',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统用户角色关系表';
+
+SET FOREIGN_KEY_CHECKS = 1;
