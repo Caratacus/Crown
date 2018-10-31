e9b546c7f0544651f34f6aff6963934fdc3a1f3f
Caratacus <330952486@qq.com>
2018-10-31 14:05:51 +0800

    Change: Prevent XSS

--- src/main/java/org/crown/common/http/wrapper/ApiRequestWrapper.java
+++ src/main/java/org/crown/common/http/wrapper/ApiRequestWrapper.java
@@ -3,6 +3,7 @@ package org.crown.common.http.wrapper;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
@@ -10,10 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
 import org.crown.common.http.RequestKit;
+import org.springframework.web.util.HtmlUtils;
+
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 
 
 /**
  * Request包装类
+ * <p>
+ * 1.预防xss攻击
+ * 2.拓展requestbody无限获取(HttpServletRequestWrapper只能获取一次)
+ * </p>
  *
  * @author Caratacus
  */
@@ -31,11 +39,15 @@ public class ApiRequestWrapper extends HttpServletRequestWrapper {
 
     @Override
     public BufferedReader getReader() {
-        return new BufferedReader(new InputStreamReader(getInputStream()));
+        ServletInputStream inputStream = getInputStream();
+        return Objects.isNull(inputStream) ? null : new BufferedReader(new InputStreamReader(inputStream));
     }
 
     @Override
     public ServletInputStream getInputStream() {
+        if (ObjectUtils.isEmpty(body)) {
+            return null;
+        }
         final ByteArrayInputStream bais = new ByteArrayInputStream(body);
         return new ServletInputStream() {
 
@@ -61,4 +73,64 @@ public class ApiRequestWrapper extends HttpServletRequestWrapper {
         };
     }
 
-} 
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] values = super.getParameterValues(name);
+        if (values == null) {
+            return null;
+        }
+        int count = values.length;
+        String[] encodedValues = new String[count];
+        for (int i = 0; i < count; i++) {
+            encodedValues[i] = htmlEscape(values[i]);
+        }
+        return encodedValues;
+    }
+
+    @Override
+    public String getParameter(String name) {
+        String value = super.getParameter(name);
+        if (value == null) {
+            return null;
+        }
+        return htmlEscape(value);
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        Object value = super.getAttribute(name);
+        if (value instanceof String) {
+            htmlEscape((String) value);
+        }
+        return value;
+    }
+
+    @Override
+    public String getHeader(String name) {
+        String value = super.getHeader(name);
+        if (value == null) {
+            return null;
+        }
+        return htmlEscape(value);
+    }
+
+    @Override
+    public String getQueryString() {
+        String value = super.getQueryString();
+        if (value == null) {
+            return null;
+        }
+        return htmlEscape(value);
+    }
+
+    /**
+     * 使用spring HtmlUtils 转义html标签达到预防xss攻击效果
+     *
+     * @param str
+     * @see org.springframework.web.util.HtmlUtils#htmlEscape
+     */
+    protected String htmlEscape(String str) {
+        return HtmlUtils.htmlEscape(str);
+    }
+
+}
