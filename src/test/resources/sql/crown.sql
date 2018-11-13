980f4f78d8d4c85c9082df7710ae1ef0357c72f3
Caratacus <330952486@qq.com>
2018-11-14 02:00:34 +0800

    Change: updated crown.sql

--- src/test/resources/sql/crown.sql
+++ src/test/resources/sql/crown.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50718
  File Encoding         : utf-8
 
- Date: 11/12/2018 23:13:58 PM
+ Date: 11/14/2018 01:58:19 AM
 */
 
 SET NAMES utf8;
@@ -42,7 +42,7 @@ DROP TABLE IF EXISTS `sys_menu_resource`;
 CREATE TABLE `sys_menu_resource` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `menu_id` int(11) DEFAULT NULL COMMENT '菜单ID',
-  `resource_id` int(11) DEFAULT NULL COMMENT '资源ID',
+  `resource_id` varchar(32) DEFAULT NULL COMMENT '资源ID',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜单资源关系表';
 
@@ -51,13 +51,20 @@ CREATE TABLE `sys_menu_resource` (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_resource`;
 CREATE TABLE `sys_resource` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(32) NOT NULL,
   `resource_name` varchar(32) DEFAULT NULL COMMENT '资源名称',
   `mapping` varchar(64) DEFAULT NULL COMMENT '路径映射',
   `method` varchar(6) DEFAULT NULL COMMENT '请求方式',
   `verify` bit(1) DEFAULT NULL COMMENT '是否需要验证',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='资源表';
+
+-- ----------------------------
+--  Records of `sys_resource`
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_resource` VALUES ('2856ae2858a7ad748c2a3ca507068800', '修改密码', '/account/password', 'PUT', b'1'), ('35cbac1c6d9e7a11faed85568093fb6b', '获取Token', '/account/token', 'POST', b'0'), ('46d3e1d55597a7943ec79f7502c7942a', '修改用户信息', '/user/info', 'PUT', b'1'), ('9291c7b9fed9d9c7f10c4f3ccca85251', '清理Token', '/account/token', 'DELETE', b'1'), ('d5a8d43937dbc7e0729cbe99a7ec3ec6', '获取用户详情', '/user/details', 'GET', b'1'), ('e523e00b15e63b23a7851e6f2847f6f5', '查询当个角色', '/role/{id}', 'GET', b'1');
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `sys_role`
@@ -72,7 +79,7 @@ CREATE TABLE `sys_role` (
   `update_time` datetime NOT NULL COMMENT '修改时间',
   `remark` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
 
 -- ----------------------------
 --  Records of `sys_role`
@@ -99,7 +106,7 @@ DROP TABLE IF EXISTS `sys_role_resource`;
 CREATE TABLE `sys_role_resource` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_id` int(11) NOT NULL COMMENT '角色ID',
-  `resource_id` int(11) NOT NULL COMMENT '菜单ID',
+  `resource_id` varchar(32) NOT NULL COMMENT '菜单ID',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色资源关系表';
 
@@ -127,7 +134,7 @@ CREATE TABLE `sys_user` (
 --  Records of `sys_user`
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_user` VALUES ('1', 'Crown', 'caratacus@qq.com', '13712345678', '0', '1', '1', '2018-11-05 17:19:05', '2018-11-05 17:19:08', 'crown', '$apr1$crown$WQ2TEXVPUJ8l6N6gm0CGv.', '0:0:0:0:0:0:0:1');
+INSERT INTO `sys_user` VALUES ('1', 'Crown', 'caratacus@qq.com', '13712345678', '0', '1', '1', '2018-11-05 17:19:05', '2018-11-13 10:55:15', 'crown', '$apr1$crown$WQ2TEXVPUJ8l6N6gm0CGv.', '0:0:0:0:0:0:0:1');
 COMMIT;
 
 -- ----------------------------
