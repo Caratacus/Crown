837b6eab0f95b97557c3e16881bf5a4d5238f80d
Caratacus <330952486@qq.com>
2018-11-21 00:40:06 +0800

    Change: added menu.html

--- src/main/java/org/crown/controller/MenuRestController.java
+++ src/main/java/org/crown/controller/MenuRestController.java
@@ -1,9 +1,21 @@
 package org.crown.controller;
 
+import java.util.List;
 
+import org.crown.common.annotations.Resources;
+import org.crown.common.api.model.responses.ApiResponses;
 import org.crown.common.framework.controller.SuperController;
-import org.springframework.stereotype.Controller;
+import org.crown.model.entity.Menu;
+import org.crown.service.IMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * <p>
@@ -13,9 +25,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author Caratacus
  * @since 2018-10-25
  */
-@Controller
-@RequestMapping("/sys/menu")
+@Api(tags = {"Menu"}, description = "菜单相关接口")
+@RestController
+@RequestMapping(value = "/menu", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Validated
 public class MenuRestController extends SuperController {
+
+    @Autowired
+    private IMenuService menuService;
+
+    @Resources
+    @ApiOperation(value = "查询所有菜单")
+    @GetMapping
+    public ApiResponses<List<Menu>> list() {
+        return success(menuService.list());
+    }
 
 }
 
