74ca276b55e497051c8d72681275f604e7839555
Caratacus <330952486@qq.com>
2018-11-22 10:25:18 +0800

    Change: print execute sql

--- src/main/java/org/crown/config/MybatisPlusConfig.java
+++ src/main/java/org/crown/config/MybatisPlusConfig.java
@@ -2,7 +2,6 @@ package org.crown.config;
 
 import org.crown.common.mybatisplus.CommonMetaObjectHandler;
 import org.crown.common.mybatisplus.MybatisPlusSqlInjector;
-import org.crown.common.mybatisplus.SqlPerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,16 +25,6 @@ public class MybatisPlusConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
-    }
-
-    /**
-     * sql打印
-     *
-     * @return
-     */
-    @Bean
-    public SqlPerformanceInterceptor sqlPerformanceInterceptor() {
-        return new SqlPerformanceInterceptor();
     }
 
     /**
