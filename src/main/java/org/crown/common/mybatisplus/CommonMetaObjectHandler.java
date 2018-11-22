8eb6400b7dfd84dba8a9b83ee5e0936cd35011f3
Caratacus <330952486@qq.com>
2018-11-22 16:55:52 +0800

    Change: updated mybatis plus version

--- src/main/java/org/crown/common/mybatisplus/CommonMetaObjectHandler.java
+++ src/main/java/org/crown/common/mybatisplus/CommonMetaObjectHandler.java
@@ -1,20 +1,11 @@
 package org.crown.common.mybatisplus;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.Optional;
 
 import org.apache.ibatis.reflection.MetaObject;
-import org.apache.ibatis.reflection.SystemMetaObject;
 import org.crown.common.http.RequestKit;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
-import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
-import com.baomidou.mybatisplus.core.metadata.TableInfo;
-import com.baomidou.mybatisplus.core.toolkit.Constants;
-import com.baomidou.mybatisplus.core.toolkit.TableInfoHelper;
-import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 
 
 /**
@@ -44,42 +35,16 @@ public class CommonMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        setFieldValByName(createTime, LocalDateTime.now(), metaObject, FieldFill.INSERT);
-        setFieldValByName(createUid, currentUid(), metaObject, FieldFill.INSERT);
-        setFieldValByName(updateTime, LocalDateTime.now(), metaObject, FieldFill.INSERT);
-        setFieldValByName(updateUid, currentUid(), metaObject, FieldFill.INSERT);
+        setInsertFieldValByName(createTime, LocalDateTime.now(), metaObject);
+        setInsertFieldValByName(createUid, currentUid(), metaObject);
+        setInsertFieldValByName(updateTime, LocalDateTime.now(), metaObject);
+        setInsertFieldValByName(updateUid, currentUid(), metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        setFieldValByName(updateTime, LocalDateTime.now(), metaObject, FieldFill.UPDATE);
-        setFieldValByName(updateUid, currentUid(), metaObject, FieldFill.UPDATE);
-    }
-
-    /**
-     * 弃用mp的填充策略,自己实现
-     *
-     * @param fieldName
-     * @param fieldVal
-     * @param metaObject
-     * @param fieldFill
-     * @return
-     */
-    public MetaObjectHandler setFieldValByName(String fieldName, Object fieldVal, MetaObject metaObject, FieldFill fieldFill) {
-        if (Objects.nonNull(fieldVal)) {
-            if (metaObject.hasSetter(fieldName) && metaObject.hasGetter(fieldName) && isFill(fieldName, metaObject, fieldFill)) {
-                metaObject.setValue(fieldName, fieldVal);
-            } else if (metaObject.hasGetter(Constants.ENTITY)) {
-                Object et = metaObject.getValue(Constants.ENTITY);
-                if (et != null) {
-                    MetaObject etMeta = SystemMetaObject.forObject(et);
-                    if (etMeta.hasSetter(fieldName) && isFill(fieldName, etMeta, fieldFill)) {
-                        etMeta.setValue(fieldName, fieldVal);
-                    }
-                }
-            }
-        }
-        return this;
+        setUpdateFieldValByName(updateTime, LocalDateTime.now(), metaObject);
+        setUpdateFieldValByName(updateUid, currentUid(), metaObject);
     }
 
     /**
@@ -93,26 +58,5 @@ public class CommonMetaObjectHandler implements MetaObjectHandler {
         }
         return uid;
     }
-
-    /**
-     * 是否填充
-     *
-     * @param fieldName  字段名
-     * @param metaObject
-     * @param fieldFill  填充策略
-     * @return
-     */
-    private boolean isFill(String fieldName, MetaObject metaObject, FieldFill fieldFill) {
-        TableInfo tableInfo = metaObject.hasGetter(OptimisticLockerInterceptor.MP_OPTLOCK_ET_ORIGINAL) ? TableInfoHelper.getTableInfo(metaObject.getValue(OptimisticLockerInterceptor.MP_OPTLOCK_ET_ORIGINAL).getClass()) : TableInfoHelper.getTableInfo(metaObject.getOriginalObject().getClass());
-        if (Objects.nonNull(tableInfo)) {
-            Optional<TableFieldInfo> first = tableInfo.getFieldList().stream().filter(e -> e.getProperty().equals(fieldName)).findFirst();
-            if (first.isPresent()) {
-                FieldFill fill = first.get().getFieldFill();
-                return fill.equals(fieldFill) || FieldFill.INSERT_UPDATE.equals(fill);
-            }
-        }
-        return false;
-    }
-
 
 }
