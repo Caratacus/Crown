77cd512c719a5628586e59d158f873df63e9ac65
Caratacus <330952486@qq.com>
2018-12-05 14:41:48 +0800

    [新增] 测试用例

--- src/test/java/org/crown/generate/MysqlGeneratorTest.java
+++ src/test/java/org/crown/generate/MysqlGeneratorTest.java
@@ -18,7 +18,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package org.crown.generator;
+package org.crown.generate;
 
 /**
  * <p>
