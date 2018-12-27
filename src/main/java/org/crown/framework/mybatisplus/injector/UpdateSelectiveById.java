37d4c97775af02ed061515ee03d3cc1729b90812
Caratacus <330952486@qq.com>
2018-12-27 15:45:26 +0800

    [升级] Mybatis plus版本

--- src/main/java/org/crown/framework/mybatisplus/injector/UpdateSelectiveById.java
+++ src/main/java/org/crown/framework/mybatisplus/injector/UpdateSelectiveById.java
@@ -40,12 +40,12 @@ public class UpdateSelectiveById extends AbstractMethod {
     public MappedStatement injectMappedStatement(Class<?> mapperClass, Class<?> modelClass, TableInfo tableInfo) {
         SqlMethod sqlMethod = SqlMethod.UPDATE_BY_ID;
         String sql = String.format(sqlMethod.getSql(), tableInfo.getTableName(),
-            sqlSet(false, false, tableInfo, ENTITY_DOT),
-            tableInfo.getKeyColumn(), ENTITY_DOT + tableInfo.getKeyProperty(),
-            new StringBuilder("<if test=\"et instanceof java.util.Map\">")
-                .append("<if test=\"et.MP_OPTLOCK_VERSION_ORIGINAL!=null\">")
-                .append(" AND ${et.MP_OPTLOCK_VERSION_COLUMN}=#{et.MP_OPTLOCK_VERSION_ORIGINAL}")
-                .append("</if></if>"));
+                sqlSet(false, false, tableInfo, ENTITY, ENTITY_DOT),
+                tableInfo.getKeyColumn(), ENTITY_DOT + tableInfo.getKeyProperty(),
+                new StringBuilder("<if test=\"et instanceof java.util.Map\">")
+                        .append("<if test=\"et.MP_OPTLOCK_VERSION_ORIGINAL!=null\">")
+                        .append(" AND ${et.MP_OPTLOCK_VERSION_COLUMN}=#{et.MP_OPTLOCK_VERSION_ORIGINAL}")
+                        .append("</if></if>"));
         SqlSource sqlSource = languageDriver.createSqlSource(configuration, sql, modelClass);
         return addUpdateMappedStatement(mapperClass, modelClass, "updateSelectiveById", sqlSource);
     }
